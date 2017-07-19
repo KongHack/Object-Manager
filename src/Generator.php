2d31f3369b0785133265e21fa3d4e652b6bfb5a3
Mitch <mitch.dodge@mhc.org>
2017-07-19 15:58:13 -0400

    Path parser like the router

--- src/Generator.php
+++ src/Generator.php
@@ -1,12 +1,17 @@
 <?php
 namespace GCWorld\ObjectManager;
 
+use GCWorld\Utilities\General;
+use phpDocumentor\Reflection\DocBlock;
+
 /**
  * Class Generator
  * @package GCWorld\ObjectManager
  */
 class Generator
 {
+    use General;
+
     const CLASS_NAME = 'GeneratedManager';
 
     protected $master_location = null;
@@ -21,15 +26,48 @@ class Generator
         $cConfig      = new Config();
         $config       = $cConfig->getConfig();
         $this->config = $config;
+        $paths        = [];
+
         foreach($this->config as $model => $definition) {
-            if(strpos($model,'ExampleModelName')===0) {
+            // Step 1, eliminate examples
+            if(strpos($model,'Object:ExampleModelName')===0) {
                 unset($this->config[$model]);
                 continue;
             }
+
+            // Step 2, handle paths
+            if($model == 'Paths') {
+                unset($definition['Example1']);
+                unset($definition['Example2']);
+                $paths = array_values($definition);
+                unset($this->config[$model]);
+                continue;
+            }
+
+            // Step 3, eliminate remaining garbage
+            if(strpos($model,'Object:')==false) {
+                unset($this->config[$model]);
+                continue;
+            }
+
+            // Remove the "Object:" part
+            unset($this->config[$model]);
+            $this->config[substr($model,7)] = $definition;
+        }
+
+        if(count($paths) > 0) {
+            $extra = $this->generateAnnotatedConfig($paths);
+            // We are using this order so that the flat file will override
+            $this->config = array_merge($extra, $this->config);
+        }
+
+        // Make sure we have trailing slashes!
+        foreach($this->config as &$definition) {
             if(array_key_exists('namespace',$definition) && strpos($definition['namespace'],-1) != '\\') {
-                $this->config[$model]['namespace'] .= '\\';
+                $definition['namespace'] .= '\\';
             }
         }
+
     }
 
     /**
@@ -163,5 +201,83 @@ class Generator
         fclose($this->open_files[$key]);
         unset($this->open_files[$key]);
         unset($this->open_files_level[$key]);
+    }
+
+
+
+    /**
+     * @return array
+     */
+    private function generateAnnotatedConfig($paths)
+    {
+        $return = [];
+
+        if (count($paths) > 0) {
+            foreach ($paths as $path) {
+                $classFiles = self::glob_recursive(rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'*.php');
+                foreach ($classFiles as $file) {
+                    $namespace = '';
+                    $className = '';
+                    $fh        = fopen($file, 'r');
+                    while (($buffer = fgets($fh)) !== false) {
+                        if (substr($buffer, 0, 9) == 'namespace') {
+                            $namespace = substr(trim($buffer), 10, -1);
+                        }
+                        if (substr($buffer, 0, 5) == 'class') {
+                            $temp      = explode(' ', $buffer);
+                            $className = $temp[1];
+                            break;
+                        }
+                    }
+                    $classString = trim('\\'.$namespace.'\\'.$className);
+                    if (class_exists($classString)) {
+                        $thisClass = new \ReflectionClass($classString);
+                        if (($comment = $thisClass->getDocComment()) !== false) {
+                            $phpDoc = new DocBlock($comment);
+                            $config = self::processTags($classString, $phpDoc);
+                            if ($config) {
+                                $return[$className] = $config;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param string                             $classString
+     * @param \phpDocumentor\Reflection\DocBlock $phpDoc
+     * @return array|bool
+     */
+    private static function processTags($classString, DocBlock $phpDoc)
+    {
+        if (!$phpDoc->hasTag('om-method')) {
+            return false;
+        }
+
+        $tmp = explode('\\',$classString);
+
+        $config = [
+            'method'    => $phpDoc->getTagsByName('om-method'),
+            'name'      => array_pop($tmp),
+            'namespace' => implode('\\',$tmp),
+            'gc'        => 0,
+        ];
+        unset($tmp);
+
+        if($phpDoc->hasTag('om-name')) {
+            $config['name'] = $phpDoc->getTagsByName('om-name');
+        }
+        if($phpDoc->hasTag('om-namespace')) {
+            $config['namespace'] = $phpDoc->getTagsByName('om-namespace');
+        }
+        if($phpDoc->hasTag('om-gc')) {
+            $config['gc'] = abs(intval($phpDoc->getTagsByName('om-gc')));
+        }
+
+        return $config;
     }
 }
